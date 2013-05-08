f99f1b1a69f3a51ae0d413c305647303f61886e1
Ondrej Sika <sika.ondrej@gmail.com>
2013-05-08 14:04:28 +0200

    update setup.py (bin, version)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "deploy",
-    version = "1.0.0",
+    version = "1.1.0",
     url = 'http://ondrejsika.com/docs/deploy',
     download_url = 'https://github.com/sikaondrej/deploy',
     license = 'GNU LGPL v.3',
@@ -13,7 +13,7 @@ setup(
     author = 'Ondrej Sika',
     author_email = 'dev@ondrejsika.com',
     packages = find_packages(),
-    scripts = ["deploy/bin/deploy", "deploy/bin/deploy-startserver", "deploy/bin/deploy-init"],
+    scripts = ["deploy/bin/deploy", "deploy/bin/deploy-startserver", "deploy/bin/deploy-init", "deploy/bin/deploy-remote"],
     install_requires = ["cryptedserver"],
     include_package_data = True,
 )
