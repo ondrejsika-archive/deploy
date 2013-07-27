1e34a69807019cf01d6c7b851af51019b69a4487
Ondrej Sika <sika.ondrej@gmail.com>
2013-07-27 22:23:20 +0200

    fix serverconf help text

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "deploy",
-    version = "1.6.1",
+    version = "1.6.2",
     url = 'http://ondrejsika.com/docs/deploy',
     download_url = 'https://github.com/sikaondrej/deploy',
     license = 'GNU LGPL v.3',
